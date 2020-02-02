002585637a76ab5158b417ff4506a7b634a41c82
unknown <aniball2@rambler.ru>
2020-02-02 19:41:42 +0300

    'add-app'

---
+++ build/precache-manifest.7aa518bd94d9e994fa1e815fa85f25cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053417c8ce79e7844e9c6c210862b96c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51654320f7e78ddb0537",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "a2addab892d400e96b06",
+    "url": "/static/css/main.93ca06e5.chunk.css"
+  },
+  {
+    "revision": "51654320f7e78ddb0537",
+    "url": "/static/js/2.f56db934.chunk.js"
+  },
+  {
+    "revision": "6ab3a04406bd9ae1991b722d9571e2fc",
+    "url": "/static/js/2.f56db934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2addab892d400e96b06",
+    "url": "/static/js/main.d8a32940.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import TableCar from './table'
+import { Service } from './service'
 
 const App = () => {
+  const service = new Service()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableCar service={service} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
